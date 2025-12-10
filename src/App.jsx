96a80a5d3a1c88a9ab5b7900aa3ee2224a047d02
BilalHassen <bilalhassen097@gmail.com>
2025-12-10 10:13:53 +0300

    Update App structure and import styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // Styles are imported in main.jsx
-
+import Home from "./pages/Home"
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <Home/>
     </div>
   )
 }
